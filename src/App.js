d054ec31d4a002b6884e6aff732a2b6a79837f71
shojol021 <shojol021@gmail.com>
2023-03-21 17:01:37 +0600

    The Journey of React started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+const obj = {
+  Name:'Abdul',
+  Phone: 'Xiaomi',
+  Price: 20000
+}
+
+const myStyle = {backgroundColor: 'white'}
+
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{obj.Name}</h1>
+      <h2>{obj.Phone}</h2>
+      <h3>{obj.Price}</h3>
+      <MyName name="Shojol" age="31" ></MyName>
+      <MyName name="Adnan" age="30" ></MyName>
+      <MyName name="Rahul" age="40" ></MyName>
     </div>
   );
+}
+
+function MyName(props) {
+  const ageAbir = 100;
+  return (
+    <div className='person'>
+      
+      <p>My name is: {props.name}</p>
+      <p>My age is: {props.age}</p>
+      <p style={{backgroundColor: 'yellow', border: '2px solid white' }}>Abir is: {ageAbir}</p>
+    </div>
+  )
 }
 
 export default App;
