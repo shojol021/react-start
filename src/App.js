f36146dfc28f9e8144f58106592f173dfcc554dc
shojol021 <shojol021@gmail.com>
2023-03-21 17:32:13 +0600

    React 1st practice done

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ const myStyle = {backgroundColor: 'white'}
 
 function App() {
   
-  
+  const obj2 = [
+    {Name: 'Shakib', Age: 34},
+    {Name: 'Tamim', Age: 35},
+    {Name: 'Taskin', Age: 29},
+    {Name: 'Liton', Age: 28}
+  ]
   return (
     <div className="App">
       <h1>{obj.Name}</h1>
       <h2>{obj.Phone}</h2>
       <h3>{obj.Price}</h3>
+
+      <h1>From Array</h1>
+      {
+        obj2.map(player=> <MyName name={player.Name} age={player.Age}></MyName>)
+      }
+      <h1>Old</h1>
       <MyName name="Shojol" age="31" ></MyName>
       <MyName name="Adnan" age="30" ></MyName>
       <MyName name="Rahul" age="40" ></MyName>
